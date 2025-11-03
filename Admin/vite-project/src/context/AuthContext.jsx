d55684f1e63845e6850eae2d1b706f7b3def6ffd
Babli Chauhan <madebybabli@gmail.com>
2025-11-03 15:03:06 +0530

    Update AuthContext.jsx

--- Admin/vite-project/src/context/AuthContext.jsx
+++ Admin/vite-project/src/context/AuthContext.jsx
@@ -3,8 +3,7 @@ import React, { createContext } from 'react'
 export const AuthDataContext = createContext();
 
 const AuthContext = ({ children }) => {
-  let serverUrl = "https://ecommerce-website-mern-1-backend.onrender.com"
-
+  let serverUrl = "https://ecommerce-website-mern-backend-6c07.onrender.com"
   let value={
     serverUrl
   }
