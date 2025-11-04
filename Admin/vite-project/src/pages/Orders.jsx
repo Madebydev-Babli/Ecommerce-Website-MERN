c8f12d5dec40a9b36c08ec1c9c371dfd4376a7f2
Babli Chauhan <madebybabli@gmail.com>
2025-11-04 20:39:09 +0530

    Update Orders.jsx

--- Admin/vite-project/src/pages/Orders.jsx
+++ Admin/vite-project/src/pages/Orders.jsx
@@ -49,10 +49,10 @@ function AdminOrders() {
       {/* Main: add left margin for sidebar */}
       <div className="flex-1 flex flex-col" style={{ marginLeft: "16rem" }}>
         <Nav />
-        <div className="flex flex-col flex-1 items-center relative px-2 sm:px-4 lg:px-8 mt-16 w-full overflow-y-auto pb-12">
+        <div className="flex flex-col flex-1 mt-5 items-center relative px-2 sm:px-4 lg:px-8 mt-16 w-full overflow-y-auto pb-12">
           {/* Animated header */}
           <motion.h2
-            className="text-2xl lg:text-3xl mt-5 font-bold mb-6 text-[#6b302c] drop-shadow-sm text-center"
+            className="text-2xl lg:text-3xl font-bold mb-6 text-[#6b302c] drop-shadow-sm text-center"
             initial={{ opacity: 0, y: -40 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.6, type: "spring" }}
