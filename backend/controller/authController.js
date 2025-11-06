e61c58c44aa30d091ea11e7e2ea6cbe609e6a74c
Babli Chauhan <madebybabli@gmail.com>
2025-11-06 20:59:22 +0530

    Update authController.js

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -86,15 +86,20 @@ const login = async (req, res) => {
 
 const logout = async (req, res) => {
     try {
-        res.clearCookie("token")
-        return res.status(200).json({ message: "Logout SuccessFully" })
+        res.clearCookie("token", {
+            httpOnly: true,
+            secure: true,
+            sameSite: "none"
+        });
+        return res.status(200).json({ message: "Logout SuccessFully" });
     } catch (error) {
         return res.status(500).json({
             success: false,
             message: `Logout error${error.message}`
-        })
+        });
     }
-}
+};
+
 
 
 
