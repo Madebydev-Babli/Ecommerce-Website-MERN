db63e9c4d7d6b88e78ca05914dd9bcf186639602
Babli Chauhan <madebybabli>
2025-12-27 00:22:34 +0530

    Add few changes

--- Admin/vite-project/src/pages/Orders.jsx
+++ Admin/vite-project/src/pages/Orders.jsx
@@ -227,7 +227,7 @@ function AdminOrders() {
                       >
                         View
                       </button>
-                    </motion.div>
+                    </motion.tr>
                   );
                 })}
               </div>
