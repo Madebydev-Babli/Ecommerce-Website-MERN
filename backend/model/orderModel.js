8c1f3ab795cdcb60497f9cc444e8eded7de58f8e
Babli Chauhan <madebybabli>
2025-12-26 16:31:17 +0530

    Add email notification on order status update

--- backend/model/orderModel.js
+++ backend/model/orderModel.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 const orderSchema = new mongoose.Schema({
     userId: {
-        type: String, // or mongoose.Schema.Types.ObjectId with ref: "User"
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: "User",
         required: true
     },
     items: {
